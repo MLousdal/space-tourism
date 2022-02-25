96f4c769f58d836f90074bdc68aefb28ebe083b1
MLousdal <74186479+MLousdal@users.noreply.github.com>
2022-02-25 14:11:30 +0100

    Static tech

---
+++ pages/technology/index.page.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+  <main class="subpage tech">
+    <div class="wrapper">
+      <header>
+        <p class="text-3 uppercase">
+          <span class="transparent">03</span>
+          SPACE LAUNCH 101
+        </p>
+      </header>
+      <section class="content-wrapper">
+        <div class="pagination">
+          <div class="page active">
+            <span>1</span>
+          </div>
+          <div class="page">
+            <span>2</span>
+          </div>
+          <div class="page">
+            <span>3</span>
+          </div>
+        </div>
+        <article>
+          <section class="content">
+            <p class="sub-title">THE TERMINOLOGY…</p>
+            <h1>LAUNCH VEHICLE</h1>
+            <p
+              class="text"
+            >A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+          </section>
+        </article>
+        <img src="@/assets/technology/image-launch-vehicle-portrait.jpg" alt="image of the moon" />
+      </section>
+    </div>
+  </main>
+</template>
--- renderer/_default.page.server.js
+++ renderer/_default.page.server.js
@@ -24,7 +24,7 @@ async function render(pageContext) {
       <head>
         <meta charset="UTF-8" />
         <link rel="icon" href="${logoUrl}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
       </head>
