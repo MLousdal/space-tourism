7914c5116369cd1b8f14f434b7618559d16c4946
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2022-03-03 17:49:16 +0100

    Working navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,61 @@
 <script setup>
 import Link from '@/renderer/Link.vue'
+import { ref, onMounted, onUnmounted } from 'vue'
+
+let showMenu = ref(true)
+const navBtn = ref(null)
+
+function mqResize() {
+  console.log('resize')
+  const mql = window.matchMedia('(max-width: 600px)')
+  if (mql.matches) {
+    console.log('matches');
+    toggleShowMenu(false)
+  } else {
+    toggleShowMenu(true)
+    console.log('else matches');
+  }
+}
+
+function toggleShowMenu(state) {
+  showMenu.value = state
+  if (state === true) {
+    document.querySelector('body').style.overflow = 'hidden'
+    navBtn.value.src = "/assets/shared/icon-close.svg"
+  }
+  if (state === false) {
+    document.querySelector('body').style.overflow = 'auto'
+    navBtn.value.src = "/assets/shared/icon-hamburger.svg"
+  }
+}
+
+function handleOutside(e) {
+  const navbar = document.querySelector('.navbar')
+  if (window.innerWidth < 800) {
+    if (!navbar.contains(e.target)) {
+      toggleShowMenu(false)
+    }
+  }
+}
+
+onMounted(() => {
+  mqResize()
+  window.addEventListener("resize", mqResize);
+  document.addEventListener('mouseup', (e) => {
+    handleOutside(e)
+  })
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", mqResize);
+});
 </script>
 
 <template>
   <div class="navbar">
     <img src="@/assets/shared/logo.svg" alt="logo" />
     <div class="line transparent"></div>
-    <nav>
+    <nav v-show="showMenu">
       <ul>
         <li>
           <Link href="/">
@@ -30,6 +79,13 @@ import Link from '@/renderer/Link.vue'
         </li>
       </ul>
     </nav>
-    <input type="image" name="nav-btn" src="/assets/shared/icon-hamburger.svg" class="nav-btn" />
+    <input
+      ref="navBtn"
+      type="image"
+      name="nav-btn"
+      src="/assets/shared/icon-hamburger.svg"
+      class="nav-btn"
+      @click="toggleShowMenu(!showMenu)"
+    />
   </div>
 </template>
